543677ef23f7b33876b79ed0229f7e4f3212a0ac
LiamKingh-TNT <commandblock365@gmail.com>
2023-12-13 20:09:59 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
